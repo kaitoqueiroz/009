6cc27e32011a1cd21391a837e0cf956fd8baaaf0
Kaito  Queiroz <kaito_queiroz@hotmail.com>
2016-10-21 03:45:47 +0000

    asfas

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -77,13 +77,13 @@
     'use strict';
 
     angular
-        .module('app.navsearch', []);
+        .module('app.pages', []);
 })();
 (function() {
     'use strict';
 
     angular
-        .module('app.pages', []);
+        .module('app.navsearch', []);
 })();
 (function() {
     'use strict';
@@ -421,6 +421,72 @@
 
 })();
 /**=========================================================
+ * Module: access-login.js
+ * Demo for login api
+ =========================================================*/
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app.pages')
+        .controller('LoginFormController', LoginFormController);
+
+    LoginFormController.$inject = ['$http', '$state'];
+    function LoginFormController($http, $state) {
+        var vm = this;
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+          // bind here all data from the form
+          vm.account = {};
+          // place the message if something goes wrong
+          vm.authMsg = '';
+
+          vm.login = function() {
+            vm.authMsg = '';
+
+            if(vm.loginForm.$valid) {
+              $http
+                .post('api/login', {username: vm.account.username, password: vm.account.password})
+                .then(function(response) {
+                  // assumes if ok, response is an object with some data, if not, a string with error
+                  // customize according to your api
+                  if ( !response.data ) {
+                    vm.authMsg = 'Login incorreto.';
+                  }else{
+                    $state.go('app.singleview');
+                  }
+                }, function(response) {
+                  console.log(response);
+                  if(response.data.error == 401){
+                    vm.authMsg = response.data.message;
+                  }else{
+                    vm.authMsg = 'Erro de conexão com o servidor.';
+                  }
+                });
+            }
+            else {
+              // set as dirty if the user click directly to login so we show the validation messages
+              /*jshint -W106*/
+              vm.loginForm.account_email.$dirty = true;
+              vm.loginForm.account_password.$dirty = true;
+            }
+          };
+
+          vm.logout = function() {
+            $http.get('sessions/logout').then(function(response) {
+              $state.go('login');
+            });
+          }
+        }
+    }
+})();
+
+/**=========================================================
  * Module: navbar-search.js
  * Navbar search toggler * Auto dismiss on ESC key
  =========================================================*/
@@ -526,72 +592,6 @@
             .val('') // Empty input
             ;
         }        
-    }
-})();
-
-/**=========================================================
- * Module: access-login.js
- * Demo for login api
- =========================================================*/
-
-(function() {
-    'use strict';
-
-    angular
-        .module('app.pages')
-        .controller('LoginFormController', LoginFormController);
-
-    LoginFormController.$inject = ['$http', '$state'];
-    function LoginFormController($http, $state) {
-        var vm = this;
-
-        activate();
-
-        ////////////////
-
-        function activate() {
-          // bind here all data from the form
-          vm.account = {};
-          // place the message if something goes wrong
-          vm.authMsg = '';
-
-          vm.login = function() {
-            vm.authMsg = '';
-
-            if(vm.loginForm.$valid) {
-              $http
-                .post('api/login', {username: vm.account.username, password: vm.account.password})
-                .then(function(response) {
-                  // assumes if ok, response is an object with some data, if not, a string with error
-                  // customize according to your api
-                  if ( !response.data ) {
-                    vm.authMsg = 'Login incorreto.';
-                  }else{
-                    $state.go('app.singleview');
-                  }
-                }, function(response) {
-                  console.log(response);
-                  if(response.data.error == 401){
-                    vm.authMsg = response.data.message;
-                  }else{
-                    vm.authMsg = 'Erro de conexão com o servidor.';
-                  }
-                });
-            }
-            else {
-              // set as dirty if the user click directly to login so we show the validation messages
-              /*jshint -W106*/
-              vm.loginForm.account_email.$dirty = true;
-              vm.loginForm.account_password.$dirty = true;
-            }
-          };
-
-          vm.logout = function() {
-            $http.get('sessions/logout').then(function(response) {
-              $state.go('login');
-            });
-          }
-        }
     }
 })();
 
@@ -2595,6 +2595,7 @@
                 $scope.entity = result.data.data;
                 $scope.entity.data = new Date($scope.entity.data.replace("-","/"));
                 $scope.entity.valor = Number($scope.entity.valor);
+                $scope.entity.frete = Number($scope.entity.frete);
                 distribuidorService.get($scope.entity.distribuidor_id).then(function(res){
                     $scope.entity.distribuidor = res.data.data.data;
                 });
